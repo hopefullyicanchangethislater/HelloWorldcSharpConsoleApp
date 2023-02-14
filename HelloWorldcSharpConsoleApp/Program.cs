abbe475c46f62029d9a7b3ad8a7b669a6b120630
hopefullyicanchangethislater <121258893+hopefullyicanchangethislater@users.noreply.github.com>
2023-02-14 11:41:59 -0500

    Add project files.

---
+++ HelloWorldcSharpConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * This
+ * is
+ * a
+ * block
+ * comment
+ */
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+string? name = Console.ReadLine();
+Console.WriteLine(value: "That is great, " + name + ",do better...");
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code!");
+
+Console.WriteLine("What else can you do with this?");
+Console.WriteLine("Everything and nothing...");
